ee9c95927507fdeb894433de3d4c0965c6ba7a3d
green <masllonishe@gmail.com>
2024-11-10 20:04:59 +0100

    docs: update readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# github-probot
+# Bounty bot 
+powered by github-probot 
 
 > A GitHub App built with [Probot](https://github.com/probot/probot) that A Probot app
+
+
 
 ## Setup
 
@@ -11,22 +14,6 @@ npm install
 # Run the bot
 npm start
 ```
-
-## Docker
-
-```sh
-# 1. Build container
-docker build -t github-probot .
-
-# 2. Start container
-docker run -e APP_ID=<app-id> -e PRIVATE_KEY=<pem-value> github-probot
-```
-
-## Contributing
-
-If you have suggestions for how github-probot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
-
-For more, check out the [Contributing Guide](CONTRIBUTING.md).
 
 ## License
 
--- handlers/pull-request.js
+++ handlers/pull-request.js
@@ -3,47 +3,47 @@ import { submitMilestones } from '../evm_commands/submit-milestone.js'
 import { getBountyById } from '../evm_commands/get-bounty-by-id.js';
 
 export const managePullRequest = async (context) => {
-    try {
-        const pr = context.payload.pull_request;
+  try {
+      const pr = context.payload.pull_request;
 
-        if (pr.merged) {
-          const bodyText = pr.body || '';
-          // Regex to match the issue URL format in PR body
-          const issueLinkRegex = /https:\/\/github\.com\/[\w-]+\/[\w-]+\/issues\/(\d+)/;
-          const match = bodyText.match(issueLinkRegex);
-      
-          if (match) {
-            let body = `This issue was resolved by PR #${pr.number}`;
-            const issueNumber = match[1];
-            console.log(`Detected issue #${issueNumber} linked in PR body.`);
+      if (pr.merged) {
+        const bodyText = pr.body || '';
+        // Regex to match the issue URL format in PR body
+        const issueLinkRegex = /https:\/\/github\.com\/[\w-]+\/[\w-]+\/issues\/(\d+)/;
+        const match = bodyText.match(issueLinkRegex);
+    
+        if (match) {
+          let body = `This issue was resolved by PR #${pr.number}`;
+          const issueNumber = match[1];
+          console.log(`Detected issue #${issueNumber} linked in PR body.`);
 
-            const issueUrl = match[0];
-            const bountyId = await getBountyId(issueUrl);
+          const issueUrl = match[0];
+          const bountyId = await getBountyId(issueUrl);
 
-            if (bountyId){
-              
-              const bountyData = await getBountyById(bountyId);
+          if (bountyId){
+            
+            const bountyData = await getBountyById(bountyId);
 
-              if (bountyData.strategyState){
+            if (bountyData.strategyState){
 
-                if (bountyData.hunter){
-                  const submittedMilestoneData = await submitMilestones(bountyId, pr.html_url);
-                  body += `\n\n🏁 Milestone has been submitted 🏁`;
-                }
+              if (bountyData.hunter){
+                const submittedMilestoneData = await submitMilestones(bountyId, pr.html_url);
+                body += `\n\n🟢 Milestone has been submitted`;
               }
             }
-
-            await context.octokit.issues.createComment({
-              owner: context.payload.repository.owner.login,
-              repo: context.payload.repository.name,
-              issue_number: issueNumber,
-              body: body,
-            });
           }
+
+          await context.octokit.issues.createComment({
+            owner: context.payload.repository.owner.login,
+            repo: context.payload.repository.name,
+            issue_number: issueNumber,
+            body: body,
+          });
         }
-    }
-    catch(err){
-        console.log(`Error: managePullRequest ${err}`)
-        return {err: err};
-    }
+      }
+  }
+  catch(err){
+      console.log(`Error: managePullRequest ${err}`)
+      return {err: err};
+  }
 }
